88d358e45f1197cd283d9d00a0300f838eea3a26
david-rosval <20b3010135@untels.edu.pe>
2024-05-14 19:58:24 -0400

    Task 11 - Get details ISBN using Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,10 +34,19 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbnParam = req.params.isbn
-  const book = books[isbnParam]
-  if (!book) return res.status(404).json({ message: "Book not found" })
-  res.send(JSON.stringify(book, null, 4))
- });
+  const getBookByIsbnPromise = new Promise((resolve, reject) => {
+    try {
+      if (!books[isbnParam]) throw new Error("Book not found")
+      resolve(books[isbnParam])
+    } catch (error) {
+      reject(error.message)
+    }
+  })
+  getBookByIsbnPromise.then(
+    bookObtained => res.send(JSON.stringify(bookObtained, null, 4)),
+    error => res.status(404).json({ message: error })
+  )
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
